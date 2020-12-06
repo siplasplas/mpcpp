0203c5cad1a5566b892709a9200fce0d1c179256
Andrzej Borucki <borucki.andrzej@gmail.com>
2020-12-06 16:10:14 +0100

    test lambda

---
+++ test/lambda.cpp
@@ -0,0 +1,11 @@
+#include "../src/mpfrcpp.h"
+#include <utestcpp/utest.h>
+
+using namespace mpfr;
+
+TEST_CASE( "Mpfr lambda" ) {
+    typedef std::function <Mpfr( Mpfr )> Flambda;
+    Flambda fl = [](Mpfr x ) {return exp(x);};
+    Mpfr x = 33;
+    Mpfr tmp = fl(x);
+}
