7c4f3b2bc90a51bc729edd835db661d9881d42b0
Andrzej Borucki <borucki.andrzej@gmail.com>
2020-12-06 16:56:15 +0100

    small typo

--- test/lambda.cpp
+++ test/lambda.cpp
@@ -5,7 +5,7 @@ using namespace mpfr;
 
 TEST_CASE( "Mpfr lambda" ) {
     typedef std::function <Mpfr( Mpfr )> Flambda;
-    Flambda fl = [](Mpfr x ) {return exp(x);};
+    Flambda fl = [](Mpfr x) {return exp(x);};
     Mpfr x = 33;
     Mpfr tmp = fl(x);
 }
